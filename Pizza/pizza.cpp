3b316f77bc7c0bf4eb5b8b405978aa12de970406
guille <guillegalor@gmail.com>
2019-02-27 17:36:27 +0100

    tabulaciones modificadas

--- Pizza/pizza.cpp
+++ Pizza/pizza.cpp
@@ -3,177 +3,200 @@
 #include <fstream>
 #include <vector>
 #include <cstdlib>
+#include <cmath>
+
+// **********************************************************************************
+// Auxiliary functions
+
+// Returns half of the divisors of num in descendent order
+std::vector<int> reverse_half_divisors(int num){
+    int square_root = (int) sqrt(num) + 1;
+    std::vector<int> result;
+
+    for (int i = square_root-1; i > 0 ; i++) {
+        if (num % i == 0)
+            result.push_back(num/i);
+    }
+
+    return result;
+}
+
+// **********************************************************************************
 
 struct Slice {
-  int first_row;
-  int first_column;
-  int last_row;
-  int last_column;
+    int first_row;
+    int first_column;
+    int last_row;
+    int last_column;
 };
 
 enum constraints_code {
-                       slice_out_of_pizza_range,
-                       not_enought_ingredients,
-                       slice_over_another_slice,
-                       slyce_is_ok
+    slice_out_of_pizza_range,
+    not_enought_ingredients,
+    slice_over_another_slice,
+    slyce_is_ok
 };
 
 class Pizza {
 
-  std::vector<std::vector<char>> pizza_matrix;
-  std::vector<std::vector<Slice*>> slice_pointer_matrix;
-  int total_rows, total_columns, ingredients_in_slice, cells_in_slice;
-  std::vector<Slice> slices;
+    std::vector<std::vector<char>> pizza_matrix;
+    std::vector<std::vector<Slice*>> slice_pointer_matrix;
+    int total_rows, total_columns, ingredients_in_slice, cells_in_slice;
+    std::vector<Slice> slices;
 
-public:
-  Pizza(char* filename){
+    public:
+    Pizza(char* filename){
 
-    std::fstream input_data(filename, std::ios_base::in);
+        std::fstream input_data(filename, std::ios_base::in);
 
-    input_data >> total_rows >> total_columns >> ingredients_in_slice >> cells_in_slice;
+        input_data >> total_rows >> total_columns >> ingredients_in_slice >> cells_in_slice;
 
-    std::vector<char> row;
-    std::vector<Slice*> slice_row;
-    char c;
+        std::vector<char> row;
+        std::vector<Slice*> slice_row;
+        char c;
 
-    for ( int i = 0; i < total_rows; i++){
-      for ( int j = 0 ; j < total_columns; j++){
-        input_data >> c;
-        row.push_back(c);
-        slice_row.push_back(nullptr);
-      }
-      pizza_matrix.push_back(row);
-      slice_pointer_matrix.push_back(slice_row);
-      row.clear();
-      slice_row.clear();
+        for ( int i = 0; i < total_rows; i++){
+            for ( int j = 0 ; j < total_columns; j++){
+                input_data >> c;
+                row.push_back(c);
+                slice_row.push_back(nullptr);
+            }
+            pizza_matrix.push_back(row);
+            slice_pointer_matrix.push_back(slice_row);
+            row.clear();
+            slice_row.clear();
+        }
+
+        input_data.close();
     }
 
-    input_data.close();
-  }
+    void printf(){
 
-  void printf(){
+        std::cout << total_rows << total_columns << ingredients_in_slice << cells_in_slice << std::endl;
 
-    std::cout << total_rows << total_columns << ingredients_in_slice << cells_in_slice << std::endl;
+        for ( int i = 0; i < total_rows; i++){
+            for ( int j = 0 ; j < total_columns; j++){
+                std::cout << pizza_matrix[i][j];
+            }
+            std::cout << std::endl;
+        }
 
-    for ( int i = 0; i < total_rows; i++){
-      for ( int j = 0 ; j < total_columns; j++){
-        std::cout << pizza_matrix[i][j];
-      }
-      std::cout << std::endl;
     }
 
-  }
+    // If true is returned this stores the position in row and col,
+    // false means there no more avaible positions.
+    bool get_next_available_position(int& row, int& col){
 
-  // If true is returned this stores the position in row and col,
-  // false means there no more avaible positions.
-  bool get_next_available_position(int& row, int& col){
+        static int next_row = 0, next_col = 0;
 
-    static int next_row = 0, next_col = 0;
-
-    for (int i = next_row; i < total_rows; ++i){
-      for(int j = next_col; j < total_columns; ++j){
-        if(!slice_pointer_matrix[i][j]) //nullptr means position isn't in a slice
-          {
-          next_row = row = i;
-          next_col = col = j;
-          return true;
-          }
-      }
-    }
-    return false;
-  }
-
-  constraints_code satisfies_constraints(Slice s)
-  {
-
-    if (s.first_row < 0 || s.first_column < 0 || s.last_row >= total_rows || s.last_column >= total_columns)
-      return constraints_code::slice_out_of_pizza_range;
-
-    // Check if slice is bigger than accepted;
-    if (((s.last_row - s.first_row + 1) * (s.last_column - s.last_row + 1)) >= cells_in_slice)
-      return constraints_code::size_bigger_than_accepted;
-
-    int num_t = 0, num_m = 0; //initialice number of tomatoes and mush in the slice.
-
-    for (int i = s.first_row ; i <= s.last_row; ++i){
-      for(int j = s.first_column; j<= s.last_column; ++j){
-
-        if (slice_pointer_matrix[i][j]) // if any of the cells is already in a slice, return false.
-          return constraints_code::slice_over_another_slice;
-
-        if (pizza_matrix[i][j] == 'M')
-          num_m++;
-        else
-          num_t++;
-
-      }
+        for (int i = next_row; i < total_rows; ++i){
+            for(int j = next_col; j < total_columns; ++j){
+                if(!slice_pointer_matrix[i][j]) //nullptr means position isn't in a slice
+                {
+                    next_row = row = i;
+                    next_col = col = j;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
-    // check the number of ingredients
-    if (num_m >= ingredients_in_slice && num_t >= ingredients_in_slice)
-      return constraints_code::not_enought_ingredients;
+    constraints_code satisfies_constraints(Slice s)
+    {
 
-    // Everything is correct, return 2.
-    return constraints_code::slyce_is_ok;
+        if (s.first_row < 0 || s.first_column < 0 || s.last_row >= total_rows || s.last_column >= total_columns)
+            return constraints_code::slice_out_of_pizza_range;
 
-  }
+        // Check if slice is bigger than accepted;
+        if (((s.last_row - s.first_row + 1) * (s.last_column - s.last_row + 1)) >= cells_in_slice)
+            return constraints_code::size_bigger_than_accepted;
 
-  // Updates positions in the slice
-  void update_pizza_matrix(Slice s){
-    for ( int i = s.first_row; i <= s.last_row; ++i)
-      for (int j = s.first_column; j <= s.last_column; ++j)
-        slice_pointer_matrix[i][j] = &s;
-  }
+        int num_t = 0, num_m = 0; //initialice number of tomatoes and mush in the slice.
 
-  bool make_largest_slice(Slice &s){
+        for (int i = s.first_row ; i <= s.last_row; ++i){
+            for(int j = s.first_column; j<= s.last_column; ++j){
 
-    // Try every possible size in descendant order
-    for (i = cells_in_slice; i > 0; --i) {
-        int s_c_ret = satisfies_constraints(s);
+                if (slice_pointer_matrix[i][j]) // if any of the cells is already in a slice, return false.
+                    return constraints_code::slice_over_another_slice;
 
-        switch (s_c_ret) {
+                if (pizza_matrix[i][j] == 'M')
+                    num_m++;
+                else
+                    num_t++;
 
-          case slice_over_another_slice:
-            //Slice is over another slice already selected.
+            }
+        }
 
-            // Check if slice is worth?
-            // if it is, update the accepted slice, (we dont have a way to know which slice is it, so mmmm)
+        // check the number of ingredients
+        if (num_m >= ingredients_in_slice && num_t >= ingredients_in_slice)
+            return constraints_code::not_enought_ingredients;
 
-            break;
+        // Everything is correct, return 2.
+        return constraints_code::slyce_is_ok;
 
-          case slice_is_ok:
-            // Slice is correct
-            return true;
-            break;
+    }
 
-          default:
+    // Updates positions in the slice
+    void update_pizza_matrix(Slice s){
+        for ( int i = s.first_row; i <= s.last_row; ++i)
+            for (int j = s.first_column; j <= s.last_column; ++j)
+                slice_pointer_matrix[i][j] = &s;
+    }
+
+    bool make_largest_slice(Slice &s){
+
+        // Try every possible size in descendant order
+        for (i = cells_in_slice; i > 0; --i) {
+            std::vector<int> half_top_divisors = reverse_half_divisors(i);
+            for (auto div : half_top_divisors){
+                int s_c_ret = satisfies_constraints(s);
+
+                switch (s_c_ret) {
+
+                    case slice_over_another_slice:
+                        //Slice is over another slice already selected.
+
+                        // Check if slice is worth?
+                        // if it is, update the accepted slice, (we dont have a way to know which slice is it, so mmmm)
+
+                        break;
+
+                    case slice_is_ok:
+                        // Slice is correct
+                        return true;
+                        break;
+
+                    default:
+
+                }
+
+            }
+        }
+    }
+
+    void calc_slices(){
+
+        int row,col;
+
+        while(get_next_available_position(row, col)){ //While there are available positions
+
+            Slice s = {row, col, row, col}; //Construct trivial slice.
+
+            if(make_largest_slice(s)){ //If the largest slice is valid.
+                slices.push_back(s);
+                update_pizza_matrix(s);
+            }
 
         }
     }
-  }
-
-  void calc_slices(){
-
-    int row,col;
-
-    while(get_next_available_position(row, col)){ //While there are available positions
-
-      Slice s = {row, col, row, col}; //Construct trivial slice.
-
-      if(make_largest_slice(s)){ //If the largest slice is valid.
-        slices.push_back(s);
-        update_pizza_matrix(s);
-      }
-
-    }
-  }
 };
 
 int main (int argc, char** argv) {
 
-  Pizza pizza (argv[1]);
+    Pizza pizza (argv[1]);
 
-  pizza.printf();
+    pizza.printf();
 
-  return 0;
+    return 0;
 }
