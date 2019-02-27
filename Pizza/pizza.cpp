023ba168c5b402f9141e09b874c0075f16afa089
Ludvins <theludvins@gmail.com>
2019-02-27 16:06:20 +0100

    create enum

--- Pizza/pizza.cpp
+++ Pizza/pizza.cpp
@@ -10,10 +10,22 @@ struct Slice {
   int last_row;
   int last_column;
 };
+enum constraints_code {
+                       slice_out_of_pizza_range,
+                       size_bigger_than_accepted,
+                       not_enought_ingredients,
+                       slice_over_another_slice,
+                       slyce_is_ok
+
+};
 
 class Pizza {
 
+
+
+
   std::vector<std::vector<char>> pizza_matrix;
+  std::vector<std::vector<Slice*>> slice_pointer_matrix;
   int total_rows, total_columns, ingredients_in_slice, cells_in_slice;
   std::vector<Slice> slices;
 
@@ -26,15 +38,19 @@ public:
     input_data >> total_rows >> total_columns >> ingredients_in_slice >> cells_in_slice;
 
     std::vector<char> row;
+    std::vector<Slice*> slice_row;
     char c;
 
     for ( int i = 0; i < total_rows; i++){
       for ( int j = 0 ; j < total_columns; j++){
         input_data >> c;
         row.push_back(c);
+        slice_row.push_back(nullptr);
       }
       pizza_matrix.push_back(row);
+      slice_pointer_matrix.push_back(slice_row);
       row.clear();
+      slice_row.clear();
     }
 
     input_data.close();
@@ -57,12 +73,14 @@ public:
   // false means there no more avaible positions.
   bool get_next_available_position(int& row, int& col){
 
-    for (int i = 0; i < total_rows; ++i){
-      for(int j = 0; j < total_columns; ++j){
-        if(pizza_matrix[i][j] != '0') //Char 0 means the position is already in a slice
+    static int next_row = 0, next_col = 0;
+
+    for (int i = next_row; i < total_rows; ++i){
+      for(int j = next_col; j < total_columns; ++j){
+        if(!slice_pointer_matrix[i][j]) //nullptr means position isn't in a slice
           {
-          row = i;
-          col = j;
+          next_row = row = i;
+          next_col = col = j;
           return true;
           }
       }
@@ -70,28 +88,24 @@ public:
     return false;
   }
 
-  // 0 - Slice is bigger than accepted.
-  // 1 - Slice has a position already in another slice.
-  // 2 - Slice is ok
-  // 3 - The ammount of ingredients isn't ok.
-  // 4 - Slice is over the margins of the pizza
-  int satisfies_constraints(Slice s)
+
+  constraints_code satisfies_constraints(Slice s)
   {
 
     if (s.first_row < 0 || s.first_column < 0 || s.last_row >= total_rows || s.last_column >= total_columns)
-      return 4;
+      return constraints_code::slice_out_of_pizza_range;
 
     // Check if slice is bigger than accepted;
     if (((s.last_row - s.first_row + 1) * (s.last_column - s.last_row + 1)) >= cells_in_slice)
-      return 0;
+      return constraints_code::size_bigger_than_accepted;
 
     int num_t = 0, num_m = 0; //initialice number of tomatoes and mush in the slice.
 
     for (int i = s.first_row ; i <= s.last_row; ++i){
       for(int j = s.first_column; j<= s.last_column; ++j){
 
-        if (pizza_matrix[i][j] == '0') // if any of the cells is already in a slice, return false.
-          return 1;
+        if (slice_pointer_matrix[i][j]) // if any of the cells is already in a slice, return false.
+          return constraints_code::slice_over_another_slice;
 
         if (pizza_matrix[i][j] == 'M')
           num_m++;
@@ -102,22 +116,22 @@ public:
     }
 
     // check the number of ingredients
-    if (num_m >= ingredients_in_slice && num_t >= ingredients_in_slice) 
-      return 3;
+    if (num_m >= ingredients_in_slice && num_t >= ingredients_in_slice)
+      return constraints_code::not_enought_ingredients;
 
     // Everything is correct, return 2.
-    return 2;
+    return constraints_code::slyce_is_ok;
 
   }
 
-  // Sets the positions in the slice as '0'
+  // Updates positions in the slice
   void update_pizza_matrix(Slice s){
     for ( int i = s.first_row; i <= s.last_row; ++i)
       for (int j = s.first_column; j <= s.last_column; ++j)
-        pizza_matrix[i][j] = '0';
+        slice_pointer_matrix[i][j] = &s;
   }
 
-  bool get_largest_slice(Slice s){
+  bool make_largest_slice(Slice s){
 
     int s_c_ret = satisfies_constraints(s);
 
@@ -151,6 +165,7 @@ public:
       // Slice is so bad it cant be helped
       return false;
       break;
+    }
   }
 
   void calc_slices(){
